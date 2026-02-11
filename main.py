0b756793739c8f43754ef2d2c164e833c2d85ecb
alptangal <alptangal@gmail.com>
2026-02-11 15:29:38 +0700

    .

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def myStyle(log_queue):
     intents = discord.Intents.default()
     client = MyClient(intents=intents)
     log_queue.put(("info", "Starting process data..."))
-    intents = discord.Intents.default()
+    # intents = discord.Intents.default()
     # client = discord.Client(intents=intents)
     # tree = app_commands.CommandTree(client)
     HEADERS = []
@@ -322,22 +322,22 @@ st.json(result)
 with st.status("Processing...", expanded=True) as status:
     placeholder = st.empty()
     logs = []
-    while (thread and thread.is_alive()) or not st.session_state.log_queue.empty():
-        try:
-            level, message = st.session_state.log_queue.get_nowait()
-            logs.append((level, message))
+    # while (thread and thread.is_alive()) or not st.session_state.log_queue.empty():
+    #     try:
+    #         level, message = st.session_state.log_queue.get_nowait()
+    #         logs.append((level, message))
 
-            with placeholder.container():
-                for lvl, msg in logs:
-                    if lvl == "info":
-                        st.write(msg)
-                    elif lvl == "success":
-                        st.success(msg)
-                    elif lvl == "error":
-                        st.error(msg)
+    #         with placeholder.container():
+    #             for lvl, msg in logs:
+    #                 if lvl == "info":
+    #                     st.write(msg)
+    #                 elif lvl == "success":
+    #                     st.success(msg)
+    #                 elif lvl == "error":
+    #                     st.error(msg)
 
-            time.sleep(0.2)
-        except queue.Empty:
-            time.sleep(0.3)
+    #         time.sleep(0.2)
+    #     except queue.Empty:
+    #         time.sleep(0.3)
 
     status.update(label="Hoàn thành!", state="complete", expanded=False)
